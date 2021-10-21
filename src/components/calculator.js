b6b2317321f3c22a5224b482ec07db3e7d92c6d1
Whoistolu <digittolu25@gmail.com>
2021-10-21 09:28:00 +0100

    Add the wireframe of the calculator

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -17,22 +17,22 @@ class Calculator extends React.Component {
           <button type="button">AC</button>
           <button type="button">+/-</button>
           <button type="button">%</button>
-          <button className="orange" type="button">&divide;</button>
+          <button className="orange-button" type="button">&divide;</button>
           <button type="button">7</button>
           <button type="button">8</button>
           <button type="button">9</button>
-          <button className="orange" type="button">&times;</button>
+          <button className="orange-button" type="button">&times;</button>
           <button type="button">4</button>
           <button type="button">5</button>
           <button type="button">6</button>
-          <button className="orange" type="button">&minus;</button>
+          <button className="orange-button" type="button">&minus;</button>
           <button type="button">1</button>
           <button type="button">2</button>
           <button type="button">3</button>
-          <button className="orange" type="button">+</button>
+          <button className="orange-button" type="button">+</button>
           <button className="double" type="button">0</button>
           <button type="button">.</button>
-          <button className="orange" type="button">=</button>
+          <button className="orange-button" type="button">=</button>
         </div>
       </div>
     );
