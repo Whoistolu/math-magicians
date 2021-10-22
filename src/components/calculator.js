974c0fbd06688d912bf3885991c3f2da36b45e33
Whoistolu <digittolu25@gmail.com>
2021-10-22 20:54:29 +0100

    Refactor with hooks  in progress

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,52 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './calculator.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Calculator = () => {
+  const [object, setObject] = useState({});
 
-  whenClicked = (e) => {
+  const whenClicked = (e) => {
     const buttonName = e.target.textContent;
-    const newObj = calculate(this.state, buttonName);
-    this.setState(newObj);
+    const newObj = calculate(object, buttonName);
+    setObject(newObj);
   };
 
-  render() {
-    const buttonNames = ['AC', '+/-', '%', '\u00F7', '7', '8', '9', '\u00D7',
-      '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  const buttonNames = ['AC', '+/-', '%', '\u00F7', '7', '8', '9', '\u00D7',
+    '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
-    const generateClassName = (button) => {
-      if (button === '\u00F7' || button === '\u00D7' || button === '-' || button === '+' || button === '=') {
-        return 'orange-button';
-      }
-
-      if (button === '0') {
-        return 'double-space';
-      }
-      return '';
-    };
-
-    const { next, total, operation } = this.state;
-    let result = '';
-    if (total) {
-      result = `${total} ${operation || ''} ${next || ''}`;
-    } else if (next) {
-      result = next;
+  const generateClassName = (button) => {
+    if (button === '\u00F7' || button === '\u00D7' || button === '-' || button === '+' || button === '=') {
+      return 'orange-button';
     }
-    return (
-      <div className="container">
-        <div className="cScreen">
-          {result}
-        </div>
-        <div className="buttons">
-          {buttonNames.map((button) => (<button key={button} className={generateClassName(button)} onClick={this.whenClicked} type="button">{button}</button>))}
-        </div>
-      </div>
-    );
+
+    if (button === '0') {
+      return 'double-space';
+    }
+    return '';
+  };
+
+  const { next, total, operation } = object;
+  let result = '';
+  if (total) {
+    result = `${total} ${operation || ''} ${next || ''}`;
+  } else if (next) {
+    result = next;
   }
-}
+  return (
+    <div className="container">
+      <div className="cScreen">
+        {result}
+      </div>
+      <div className="buttons">
+        {buttonNames.map((button) => (<button key={button} className={generateClassName(button)} onClick={whenClicked} type="button">{button}</button>))}
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
