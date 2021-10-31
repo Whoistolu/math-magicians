50e9d55f18e4c668abc19845bccc588fb8368621
Whoistolu <digittolu25@gmail.com>
2021-10-31 22:44:29 +0100

    Style the Quote

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -4,7 +4,12 @@ import styles from './Quote.module.css';
 const Quote = () => (
   <p className={styles.quote}>
     Mathematics is not about numbers, equations, computations or algorithm:
-    It is about understanding- Williams Paul Thurston.
+    It is about understanding — Williams Paul Thurston.
+
+    We will always have STEM with us. Some things will drop out of the public eye and go away,
+    but there will always be science, engineering, and technology. And there will always,
+    always be mathematics.
+    — Katherine Johnson, African-American mathematician
   </p>
 );
 
